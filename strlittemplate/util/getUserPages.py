7f43c9e104b1e9e204677335922b32332fefcc94
2459354M <2459354M@student.gla.ac.uk>
2022-01-08 15:41:03 +0000

    updated gitlab repo

---
+++ strlittemplate/util/getUserPages.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import inspect
+
+
+##KetZoomer - https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir) 
+
+
+import userPages
+print("success importing userPages")
