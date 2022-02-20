0cd05515d3d33ac1d397f3fe91c14f394b97a30f
2459354M <2459354M@student.gla.ac.uk>
2022-02-20 13:50:23 +0000

    updated gitlab repo

--- userPages/themeC/page1.py
+++ userPages/themeC/page1.py
@@ -1,24 +1,15 @@
+from email.policy import default
+from matplotlib.pyplot import get
 import streamlit as st
 from core.ThemePage import Page
 import core.MultiApp as hub
+from strlittemplate.util import Flattenjson, Value
 # custom
+import numpy as np
 import pandas as pd
 #####################
 # main part
 #####################
-
-
-def listkey(mylist, mykey, component=0):
-    keys = [mykey]
-    if type(mylist[mykey]) == dict:
-        sub_key = st.selectbox("Select y component:",
-                               mylist[mykey], key=component)
-        keys.extend(listkey(mylist[mykey], sub_key))
-    elif type(mylist[mykey]) == list:
-        sub_key = st.selectbox("Select y component:", range(
-            len(mylist[mykey])), key=component)
-        keys.extend(listkey(mylist[mykey], sub_key))
-    return keys
 
 
 class Page1(Page):
@@ -31,9 +22,19 @@ class Page1(Page):
         if data:
             one_data = data[0]
 
-            datakeys = one_data.keys()
+            flatdatakeys = Flattenjson.flattenjson(one_data)
+            stringkeys = list(flatdatakeys.keys())
+            stringkeys.insert(0, "none")
 
-            index1 = st.selectbox("Select y component:", datakeys)
-            firstkey = listkey(one_data, index1, component=1)
-            index2 = st.selectbox("Select x component:", datakeys)
-            secondkey = listkey(one_data, index2, component=2)
+            index1 = st.selectbox("Select y component:", stringkeys, index=0)
+            index2 = st.selectbox("Select x component:", stringkeys, index=0)
+
+            value1, value2 = [], []
+            if index1 != "none" and index2 != "none":
+                for i in range(len(data)):
+                    value1.append(Value.getValue(
+                        data[i], flatdatakeys, index1))
+                    value2.append(Value.getValue(
+                        data[i], flatdatakeys, index2))
+                st.write(Value.count(value1))
+                st.write(Value.count(value2))
