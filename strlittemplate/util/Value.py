5285c115478db919fe73f1fccf29df5c89d8226e
2459354M <2459354M@student.gla.ac.uk>
2022-03-02 15:59:06 +0000

    updated gitlab repo

--- strlittemplate/util/Value.py
+++ strlittemplate/util/Value.py
@@ -24,3 +24,25 @@ def count(l):
             
     return d
 
+def getPairings(value1,value2):
+    mylist = value1.copy()
+    mylist.extend(value2)
+    mylist = list(set(mylist))
+    source = []
+    for i in value1:
+        source.append(mylist.index(i))
+
+    target = []
+    for i in value2:
+        target.append(mylist.index(i))
+
+    a = zip(source, target)
+
+    d = count(a)
+    source, target, value = [], [], []
+    for i in d.keys():
+        source.append(i[0])
+        target.append(i[1])
+        value.append(d[i])
+
+    return mylist,source,target,value
